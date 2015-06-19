d0ebaf63c4adc3e6f3ca30da263c7b40972cdcf3
Jay <jay.lecavalier@colorado.edu>
2015-06-18 19:01:00 -0600

    textured fenceposts. Also lookin' good.

--- src/objects/fencepost.c
+++ src/objects/fencepost.c
@@ -8,17 +8,27 @@ void fencepost(double x,double y,double z,
   glTranslated(x,y,z);
   glScaled(dx,dy,dz);
   // Fencepost
-  glBegin(GL_QUADS);
   glColor3f(1,1,1);
+  glBegin(GL_QUADS);
+  // Normal points straight forward
+  glNormal3d(0,0,1);
   // Fencepost base
+  glTexCoord2d(1,0);
   glVertex3f(.25,0,0);
+  glTexCoord2d(0,0);
   glVertex3f(0,0,0);
+  glTexCoord2d(0,1);
   glVertex3f(0,.75,0);
+  glTexCoord2d(1,1);
   glVertex3f(.25,.75,0);
   // Fencepost cap
+  glTexCoord2d(0,1);
   glVertex3f(.25,.75,0);
+  glTexCoord2d(0,0);
   glVertex3f(0,.75,0);
+  glTexCoord2d(.48,1);
   glVertex3f(.124,1,0);
+  glTexCoord2d(.52,1);
   glVertex3f(.126,1,0);
   //End
   glEnd();
